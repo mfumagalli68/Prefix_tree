df5b020258d15a6b199d2aef61f1724f34a14bdc
m.fumagalli68 <m.fumagalli68@gmail.com>
2017-12-31 00:28:22 +0100

    a

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ trie = prefix.Trie()
 #pass data
 data=pandas.read_csv('')
 
+#execute saax representation from R (seewave package). I don't manage to install sax.py package
 command = 'Rscript'
 path2script = 'path/to your script/max.R'
 
@@ -16,10 +17,11 @@ args = ['11', '3', '9', '42']
 # Build subprocess command
 cmd = [command, path2script] + args
 
-# check_output will run the command and store to result
+# check_output will run the command and store to result. saax_repr: saax_representation of time series.
 saax_repr = subprocess.check_output(cmd, universal_newlines=True)
+#join in one str
 saax_repr= [''.join(i) for i in saax_repr]
-#serie_numerica serie numerica in differenze da prendere dal dataset
+#serie_numerica: time series in difference
 trie.insert(saax_repr,serie_numerica,3)
 
 #predictions
